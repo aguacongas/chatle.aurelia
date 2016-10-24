5bf952435b1261b0b32e734105ec54cbbe8c3abb
Olivier Lefebvre <olivier.lefebvre@live.com>
2016-10-24 23:31:51 +0200

    copy from feature

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -2,8 +2,9 @@ import { HttpClient } from 'aurelia-http-client';
 import { autoinject } from 'aurelia-framework';
 
 import { Settings } from '../config/settings';
-import { State } from './state'
-import { Helpers } from './helpers'
+import { State } from './state';
+import { Helpers } from './helpers';
+import { ManageLogins } from '../model/manage-logins'
 
 import { ChangePassword } from '../model/changePassword';
 
@@ -15,9 +16,9 @@ export class AccountService {
         private state: State,
         private helpers: Helpers) { }
 
-    changePassword(model: ChangePassword): Promise<any> {
+    changePassword(model: ChangePassword): Promise<void> {
         if (this.state.isGuess) {            
-            return new Promise<any>((resolve, reject) => {
+            return new Promise<void>((resolve, reject) => {
                 this.http.post(this.settings.accountdAPI + '/setpassword', model)
                     .then(response => {
                         this.state.isGuess = false;
@@ -27,7 +28,7 @@ export class AccountService {
                     .catch(error => reject(this.helpers.getError(error)));
             });
         } else {
-            return new Promise<any>((resolve, reject) => {
+            return new Promise<void>((resolve, reject) => {
                 this.http.put(this.settings.accountdAPI + '/changepassword', model)
                     .then(response => resolve())
                     .catch(error => reject(this.helpers.getError(error)));
@@ -35,4 +36,23 @@ export class AccountService {
         }
     }
 
+    getLogins(): Promise<ManageLogins> {
+        return new Promise<ManageLogins>((resove, reject) => {
+            this.http.get(this.settings.accountdAPI + '/logins')
+                .then(response => {                    
+                    resove(response.content as ManageLogins);
+                })
+                .catch(error => reject(new Error('The service is down')));
+        });
+    }
+
+    removeLogin(loginProvider: string, providerKey: string): Promise<void> {
+        return new Promise<void>((resolve, reject) => {
+            this.http.delete(this.settings.accountdAPI + '/sparemoveLogin?loginProvider=' + encodeURIComponent(loginProvider) + '&providerKey=' + encodeURIComponent(providerKey))
+                .then(() => {
+                    resolve();
+                })
+                .catch(e => reject(this.helpers.getError(e)));
+        });
+    }
 }
