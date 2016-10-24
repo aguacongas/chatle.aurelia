3a4bf9e9523103309849fbc15936137f77a305ad
Olivier Lefebvre <olivier.lefebvre@live.com>
2016-10-24 23:31:01 +0200

    copy from feature

--- src/pages/account.ts
+++ src/pages/account.ts
@@ -1,93 +1,71 @@
 import { autoinject } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
 import { EventAggregator, Subscription } from 'aurelia-event-aggregator';
-import { ValidationRules } from 'aurelia-validation';
-import { ValidationControllerFactory, ValidationController} from 'aurelia-validation';
 
 import { ConnectionState } from '../services/chat.service';
 import { State } from '../services/state';
 import { AccountService } from '../services/account.service';
+import { LoginService } from '../services/login.service';
 import { ChangePassword } from '../model/changePassword';
+import { ManageLogins } from '../model/manage-logins';
 import { ConnectionStateChanged } from '../events/connectionStateChanged';
+import { Settings } from '../config/settings';
 
 @autoinject
 export class Account {
     userName: string;
-    oldPassword: string;
-    newPassword: string;
-    confirmPassword: string;    
     errorMessage: string;
-    isGuess: boolean;
-    controller: ValidationController;
+    logins: ManageLogins;
+    externalLinkLogin: string;
+    token: string;
 
     private connectionStateSubscription: Subscription;
 
-    constructor(private service: AccountService, 
+    constructor(private accountService: AccountService, 
+            private loginService: LoginService,
             private router: Router, 
             private ea: EventAggregator,
             private state: State,
-            controllerFactory: ValidationControllerFactory) { 
+            settings: Settings) { 
         this.userName = state.userName;
-        this.isGuess = state.isGuess;
-        this.controller = controllerFactory.createForCurrentScope();                
+        this.externalLinkLogin = settings.apiBaseUrl + 
+            settings.accountdAPI + 
+            '/linklogin?returnUrl=' + 
+            encodeURIComponent(location.protocol + '//' + location.host + '/manage');
+    }
+
+    remove(loginProvider: string, providerKey: string) {
+        this.accountService.removeLogin(loginProvider, providerKey)
+            .then(() => {
+                let currentLogins = this.logins.currentLogins;
+                const index = currentLogins.findIndex(value => value.loginProvider === loginProvider && value.providerKey === providerKey);
+                currentLogins.splice(index, 1);
+            })
+            .catch((e: Error) => this.errorMessage = e.message);
     }
 
     attached() {
         this.connectionStateSubscription = this.ea.subscribe(ConnectionStateChanged, e => {
             if ((<ConnectionStateChanged>e).state === ConnectionState.Connected) {
-                this.isGuess = this.state.isGuess;
+                this.getLogins();
             }
         });
+        
+        this.getLogins();
     }
 
     detached() {
         this.connectionStateSubscription.dispose();
     }
 
-    changePassword() {
-        this.controller.validate();
-
-        if (this.controller.errors.length === 0) {
-            let model = <ChangePassword> {
-                    oldPassword: this.oldPassword,
-                    newPassword: this.newPassword, 
-                    confirmPassword: this.confirmPassword
-                };
-
-            this.service.changePassword(model)
-                .then(() => {
-                    this.isGuess = false;
-                    this.router.navigateToRoute('home');
+    private getLogins() {
+        this.loginService.getXhrf()            
+            .then((token: string) => {
+                    this.token = token;
+                    this.accountService.getLogins()
+                        .then((logins: ManageLogins) => this.logins = logins)
+                        .catch((e: Error) => this.errorMessage = e.message);
                 })
-                .catch(error => this.errorMessage = error);
-        }
+            .catch((e: Error) => this.errorMessage = e.message);
     }
 }
-
-ValidationRules.customRule(
-    'matchesProperty',
-    (value, obj, otherPropertyName) => 
-        value === null
-        || value === undefined
-        || value === ''
-        || obj[otherPropertyName] === null
-        || obj[otherPropertyName] === undefined
-        || obj[otherPropertyName] === ''
-        || value === obj[otherPropertyName],
-    '${$displayName} must match ${$config.otherPropertyName}',
-    otherPropertyName => ({ otherPropertyName }));
-
-        
-ValidationRules
-    .ensure((a: Account) => a.confirmPassword)
-        .displayName('Confirm new password')
-        .required()
-        .satisfiesRule('matchesProperty', 'newPassword')
-            .withMessage('Confirm new password must match New password')
-    .ensure((a: Account) => a.newPassword)
-        .displayName("New password")
-        .required()
-        .matches(/(?=.*[A-Z])(?=.*[!@#$&\.\*\-\+\=\?£€])(?=.*[0-9])/)
-            .withMessage('${$displayName} must contains at least one uppercase letter, one digit and one special charactere.')
-        .minLength(6)
-    .on(Account);
