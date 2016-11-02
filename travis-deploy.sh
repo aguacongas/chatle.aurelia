926ecb1e8619f9bb2149572ee48669c1d69a8bd3
olefebvre <olefebvre@cityway.fr>
2016-11-02 16:30:33 +0100

    update travis build

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -11,8 +11,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 fi
 
 # Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+REPO=`https://$GH_TOKEN@github.com/aguacongas/chatle.aurelia.git`
 SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
@@ -31,7 +30,7 @@ cp wwwroot/scripts/*.js out/scripts
 # Now let's go have some fun with the cloned repo
 cd out
 git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config user.email "aguacongas@gmail.com"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if [ -z `git diff --exit-code` ]; then
@@ -44,15 +43,5 @@ fi
 git add .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../id_rsa.enc -out ../id_rsa -d
-chmod 600 ../id_rsa
-eval `ssh-agent -s`
-ssh-add ../id_rsa
-
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push $TARGET_BRANCH
