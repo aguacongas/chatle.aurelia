f11e4d425a30d4d2749cce3ec084fb2d2d4ce60f
Lefebvre Olivier <LefebvreO@ne.ch>
2017-07-19 18:04:26 +0200

    use signalr-client instead of signalr

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -2,6 +2,8 @@
 import { HttpClient } from 'aurelia-http-client';
 import { autoinject } from 'aurelia-framework';
 import { Router } from 'aurelia-router';
+import { HubConnection, HttpConnection } from 'signalr-client';
+
 import environment from '../environment';
 
 import { Settings } from '../config/settings';
@@ -19,22 +21,6 @@ import { UserConnected } from '../events/userConnected';
 import { UserDisconnected } from '../events/userDisconnected';
 import { ServiceError } from '../model/serviceError';
 
-interface ChatSignalR extends SignalR {
-    chat: ChatProxy,
-    hub: any
-}
-
-interface ChatProxy {
-    client: ChatClient
-}
-
-interface ChatClient {
-    userConnected: (user: User) => void;
-    userDisconnected: (user: Disconnected) => void;
-    messageReceived: (message: Message) => void;
-    joinConversation: (conversation: Conversation) => void;
-}
-
 export interface Disconnected {
     id: string;
     isRemoved: boolean
@@ -51,6 +37,7 @@ export class ChatService {
     currentState = ConnectionState.Disconnected;    
     currentConversation: Conversation;
 
+    private hubConnection: HubConnection;
     constructor(private settings: Settings, 
         private ea: EventAggregator, 
         private http: HttpClient, 
@@ -58,73 +45,46 @@ export class ChatService {
         private helpers: Helpers) { }
     
     start(): Promise<ConnectionState> {
-        
-        let debug = environment.debug;
-        // only for debug
-        let hub = jQuery.connection.hub; 
-        hub.logging = debug;
-        hub.url = this.settings.apiBaseUrl + '/signalr';
-        // get the signalR hub named 'chat'
-        let connection = <ChatSignalR>jQuery.connection;
-        let chatHub = connection.chat;
-        
+        const connection = new HttpConnection(this.settings.apiBaseUrl + this.settings.chatAPI);
+        this.hubConnection = new HubConnection(connection);
         /**
           * @desc callback when a new user connect to the chat
           * @param User user, the connected user
         */
-        chatHub.client.userConnected = user => this.onUserConnected(user);
+        this.hubConnection.on('userConnected', user => this.onUserConnected(user));
         /**
           * @desc callback when a new user disconnect the chat
           * @param id, the disconnected user id
         */
-        chatHub.client.userDisconnected = user => this.onUserDisconnected(user);
+        this.hubConnection.on('userDisconnected', user => this.onUserDisconnected(user));
         /**
           * @desc callback when a message is received
           * @param String to, the conversation id
           * @param Message data, the message
         */
-        chatHub.client.messageReceived = message => this.onMessageReceived(message);
+        this.hubConnection.on('messageReceived', message => this.onMessageReceived(message));
         /**
           * @desc callback when a new conversation is create on server
           * @param Conv data, the conversation model
         */
-        chatHub.client.joinConversation = conversation => this.onJoinConversation(conversation);
+        this.hubConnection.on('messageReceived', conversation => this.onJoinConversation(conversation));
 
-        if (debug) {
-            // for debug only, callback on connection state change
-            hub.stateChanged(change => {
-                let oldState: string,
-                    newState: string;
-                
-                let signalR = jQuery.signalR;
-                for (var state in signalR.connectionState) {
-                    if (signalR.connectionState[state] === change.oldState) {
-                        oldState = state;
-                    }
-                    if (signalR.connectionState[state] === change.newState) {
-                        newState = state;
-                    }
-                }
-
-                console.log("Chat Hub state changed from " + oldState + " to " + newState);
-            });                        
+        this.hubConnection.onClosed = e => {
+            if (e) {
+                this.onError(e);
+            } else {
+                this.onDisconnected();
+            }
         }
-
-        // callback on connection reconnect
-        hub.reconnected(() => this.onReconnected());
-        // callback on connection error
-        hub.error(error => this.onError(error) );
-        // callback on connection disconnect
-        hub.disconnected(() => this.onDisconnected());
     
         // start the connection
         return new Promise<ConnectionState>((resolve, reject) => {
-            hub.start()
-                .done(() => { 
+            this.hubConnection.start()
+                .then(() => { 
                     this.setConnectionState(ConnectionState.Connected);
                     resolve(ConnectionState.Connected);
                 })
-                .fail(error => {
+                .catch(error => {
                     this.setConnectionState(ConnectionState.Error)
                     reject(new Error(error));
                 });
@@ -132,7 +92,7 @@ export class ChatService {
     }
 
     stop() {
-        jQuery.connection.hub.stop();
+        this.hubConnection.stop();
     }
 
     private setConnectionState(connectionState: ConnectionState) {
